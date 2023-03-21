1623fdc5d12df3d43f9bbfec3c11a48b68597151
istomyang <istomyang@gmail.com>
2023-03-21 19:19:11 +0800

    fix: use wrong variable
    to assign current container runner.

--- src/container/redis/run.sh
+++ src/container/redis/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+S_CONTAINER_CMD="docker"
+
 S_CONTAINER_REDIS_SHELL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P && cd - >>/dev/null || exit)"
 S_CONTAINER_REDIS_DB_DIR="$S_CONTAINER_REDIS_SHELL_DIR/db"
 S_CONTAINER_REDIS_CONF="$S_CONTAINER_REDIS_SHELL_DIR/redis7.conf"
@@ -14,8 +16,8 @@ function s_container_redis_run() {
   local conf="$S_CONTAINER_REDIS_CONF:/opt/bitnami/redis/mounted-etc/redis.conf"
 
   local network_name="redis-network"
-  if ! docker network ls | grep -iq $network_name; then
-    docker network create $network_name
+  if ! "$S_CONTAINER_CMD" network ls | grep -iq $network_name; then
+    "$S_CONTAINER_CMD" network create $network_name
   fi
 
   local image=${S_CONTAINER_REDIS_IMAGE:-"bitnami/redis:latest"}
@@ -23,8 +25,8 @@ function s_container_redis_run() {
   local name_client="$name-client"
   local name_server="$name-server"
 
-  if ! docker ps -a | grep -iq "$name_server"; then
-    docker run -d --name "$name_server" \
+  if ! "$S_CONTAINER_CMD" ps -a | grep -iq "$name_server"; then
+    "$S_CONTAINER_CMD" run -d --name "$name_server" \
       -e ALLOW_EMPTY_PASSWORD=yes \
       -p 6379:6379 \
       -v "$vol" \
@@ -32,11 +34,11 @@ function s_container_redis_run() {
       --network $network_name \
       "$image"
   else
-    docker start "$name_server" >/dev/null
+    "$S_CONTAINER_CMD" start "$name_server" >/dev/null
   fi
 
   while true; do
-    docker run -it --rm \
+    "$S_CONTAINER_CMD" run -it --rm \
       --name "$name_client" \
       --network $network_name \
       "$image" redis-cli -h "$name_server" 2>/dev/null && break
@@ -47,5 +49,5 @@ function s_container_redis_run() {
 
 function s_container_redis_remove() {
   local name=${S_CONTAINER_REDIS_NAME:-"my-redis"}
-  docker stop "$name-server" >/dev/null && echo "INFO: Success remove container"
+  "$S_CONTAINER_CMD" stop "$name-server" >/dev/null && echo "INFO: Success remove container"
 }
